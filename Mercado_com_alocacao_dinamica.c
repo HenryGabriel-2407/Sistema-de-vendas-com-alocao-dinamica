f79fde1c5fb2740eb309cbfcd437386c14e3b1c6
Henry Gabriel <63942305+HenryGabriel-2407@users.noreply.github.com>
2023-06-20 15:18:06 -0300

    Update Mercado_com_alocacao_dinamica.c

--- Mercado_com_alocacao_dinamica.c
+++ Mercado_com_alocacao_dinamica.c
@@ -4,7 +4,6 @@ void bemvindo(){
     printf("Bem vindo a Festa Junina Uniavan\n");
     printf("Digite em...\n1 - Iniciar compras\n2 - Ver lista de produtos\n3 - Sair\nDigite: ");
 }
-//bem- vindo
 void produtos(){
     printf("\nCodigo\t\tComidas\t\tPreco\n");
     printf("0 - Cachorro quente.........RS5.00\n");
@@ -18,7 +17,6 @@ void produtos(){
     printf("8 - Churrasco no espeto.....RS9.90\n");
     printf("9 - Mousse de Cupuacu.......RS2.50\n\n");
 }
-// produtos
 int main(){
     int escolha;
     float opcoes[10] = {5.00, 3.50, 4.90, 8.70, 12.00, 6.00, 10.00, 7.80, 9.90, 2.50};
@@ -28,8 +26,6 @@ int main(){
         scanf("%d", &escolha);
         if (escolha == 1){
             produtos();
-            int*num;
-            num = malloc(sizeof(int));
             int*quantidade;
             quantidade = malloc(sizeof(int));
             int codigo;
@@ -44,17 +40,15 @@ int main(){
                 scanf("%d", &codigo);
                 printf("Digite a quantidade do %s :", nome[codigo]);
                 scanf("%d", &quantidade[i]);
-                printf("%.2f\n", opcoes[codigo]* quantidade[i]);
                 valor[i] = opcoes[codigo]*quantidade[i];
                 total += valor[i];
-                num[i]= nome[codigo];
-                printf("Quer continuar? [1 - SIM / 0 - NAO ]: ");
+                printf("\nQuer continuar? [1 - SIM / 0 - NAO ]: ");
                 scanf("%d", &b);
                 if (b == 1){
                     ++a;
                     continue;
                 }
-                printf("Quer excluir produto? [1 - SIM / 0 - NAO ]: ");
+                printf("\nQuer excluir produto? [1 - SIM / 0 - NAO ]: ");
                 scanf("%d", &b);
                 if (b == 1) {
                     printf("Digite o numero do item do produto a ser excluido: ");
@@ -82,17 +76,14 @@ int main(){
                             total -= valor[codigo-1];
                             valor[codigo-1] = opcoes[codigo-1] * nova_quantidade;
                             total += valor[codigo-1];
-                            quantidade = nova_quantidade;
+                            quantidade[codigo-1] = nova_quantidade;
                         }
-                        
                     }
                 }
-                
-                
                 printf("\n---------------------COMANDA---------------------\n");
                 for (int j = 0; j <= a; j++){
                     if (quantidade[j] > 0){
-                            printf("%d\t%d - %s - %.2f\n",j+1, quantidade[j], num[j], valor[j]);
+                            printf("%d\t%d - %s - %.2f\n",j+1, quantidade[j], nome[j], valor[j]);
                     }
                 }
             }
@@ -105,7 +96,6 @@ int main(){
         else{
             printf("Adeus!!!!\n");
             break;
-        }
-    
+        }   
     }while(escolha == 1 || escolha == 2);
 }
